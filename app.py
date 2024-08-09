6f73a1e37121fce00b193a8bff695b914b62b312
Kavya Sri Relangi <149765449+kavyasri2099@users.noreply.github.com>
2024-08-10 04:16:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,10 @@
 import os
-import sounddevice as sd
 import streamlit as st
 from langchain.memory import ConversationBufferMemory
-from scipy.io import wavfile
-import numpy as np
-
 from utils import record_audio_chunk, transcribe_audio, get_response_llm, play_text_to_speech, load_whisper
 
 chunk_file = 'temp_audio_chunk.wav'
 model = load_whisper()
-
-def record_audio_chunk(duration=5):
-    """Record audio for a specified duration and save to a file."""
-    print("Recording...")
-    sample_rate = 16000
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
-    sd.wait()  # Wait until recording is finished
-
-    # Convert to numpy array
-    audio_data = audio_data.flatten()
-    
-    # Check if the recorded chunk contains silence
-    if is_silence(audio_data):
-        return None
-    
-    # Save audio data to a temporary file
-    wavfile.write(chunk_file, sample_rate, audio_data)
-    return chunk_file
-
-def is_silence(data, max_amplitude_threshold=3000):
-    """Check if audio data contains silence."""
-    max_amplitude = np.max(np.abs(data))
-    return max_amplitude <= max_amplitude_threshold
 
 def main():
     st.markdown('<h1 style="color: darkblue;">AI Voice Assistant️</h1>', unsafe_allow_html=True)
@@ -60,7 +33,6 @@ def main():
                 play_text_to_speech(text=response_llm)
             else:
                 break  # Exit the while loop
-
         print("End Conversation")
 
 if __name__ == "__main__":
